05baaa95ec5f259f800482d3dc5b048e84dc276a
tobias <tobias.omondi@student.moringaschool.com>
2026-01-26 06:29:07 +0300

    urls and views files testing

---
+++ backend/myproject/urls.py
@@ -0,0 +1,20 @@
+# urls for dental ops app
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ClinicViewSet, PatientViewSet , AppointmentViewSet, TreatmentViewSet, CommunicationViewSet, SubscriptionViewSet, ClinicAdminViewSet
+
+router = DefaultRouter()
+
+# register viewsets with the router
+router.register(r'clinics', ClinicViewSet, basename='clinic')
+router.register(r'patients', PatientViewSet,)
+router.register(r'appointments', AppointmentViewSet)
+router.register(r'treatments', TreatmentViewSet)
+router.register(r'communications', CommunicationViewSet)
+router.register(r'clinic-admins', ClinicAdminViewSet, basename='clinic-admin')
+router.register(r'subscriptions', SubscriptionViewSet)
+
+
+urlPatterns = [
+  path('', include(router.urls)),
+]
--- backend/myproject/views.py
+++ backend/myproject/views.py
@@ -1,7 +1,44 @@
-# # views for myproject
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from .models import MyModel
+# views for myproject
+from django.shortcuts import render
+# from django.http import HttpResponse , JsonResponse, Http404 , HttpResponseRedirect, HttpRequest, HttpResponseForbidden, HttpResponseNotAllowed
+from rest_framework import viewsets
+from .serializer import ClinicalDataSerializer, PatientDataSerializer, AppointmentDataSerializer, TreatmentDataSerializer , CommunicationDataSerializer, SubscriptionDataSerializer
 
-# def index(request):
+from .models import Clinic, Patient, Appointment, Treatment_Plan, Communication, Clinic_Admin, Subscription
+
+# --CLINIC VIEW SET
+class ClinicViewSet(viewsets.ModelViewSet):
+  queryset = Clinic.objects.filter(active = True).order_by('name') # show onlt active clinics
+  serializer_class = ClinicalDataSerializer
+
+#  --PATIENT VIEW SET--
+class PatientViewSet (viewsets.ModelViewSer):
+  queryset = Patient.objects.all().order_by('name') #show all patients ordered by name
+  serializer_class = PatientDataSerializer
+
+# -- APPOINTMENT VIEW SET --
+class AppointmentViewSet (viewsets.ModelViewSet):
+  queryset = Appointment.objects.all().order_by('appointment_date')
+  serializer_class = AppointmentDataSerializer
+
+# ==TREATMENT VIEW SET==
+class TreatmentViewSet (viewsets.ModelViewSet):
+  queryset = Treatment_Plan.objects.all().order_by('treatment_name')
+  serializer_class =TreatmentDataSerializer
+
+# == COMMUNICATION VIEW SET==
+class CommunicationViewSet (viewsets.ModelViewSet):
+  queryset = Communication.objects.all().order_by('sent_at')
+  serializer_class = CommunicationDataSerializer
+
+# == CLINIC ADMIN VIEW SET ==
+class ClinicAdminViewSet (viewsets.ModelViewSet):
+  queryset = Clinic_Admin.objects,all()
+  serializer_class = ClinicalDataSerializer
+
+# == subscription view set ==
+class SubscriptionViewSet (viewsets.ModelViewSet):
+  queryset = SubscriptionDataSerializer.abjects.all()
+  serializer_class = SubscriptionDataSerializer
   
+# ==CREATE OTHER VIEWS IF NEEDED==
